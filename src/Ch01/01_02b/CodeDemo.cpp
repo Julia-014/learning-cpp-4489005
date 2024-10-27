3b1b494e762e53c32d2a6ac6ede3c8ac84936f3e
Julia-014 <74207689+Julia-014@users.noreply.github.com>
2024-10-27 22:13:57 +0000

    added hello world code

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+int main(int argc, char * argv[])
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
